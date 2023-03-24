2b77536511d8a3d9498394a8404714c3e213e3f0
ioannis <ioanniscatargiu@outlook.com>
2023-03-24 18:04:55 +0200

    test3

--- src/com/unipi/mainpackage/Main.java
+++ src/com/unipi/mainpackage/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("hello world");
 	    System.out.println("test");
+	    System.out.println("test");
+	    System.out.println("test");
+	    System.out.println("test");
     }
 }
