1d607b5e637bbbcd92b0f423f4c0893793972a18
Kostas <Kostas@DESKTOP-SBD0L02.home>
2023-04-04 00:55:38 +0300

    Dn dexete ena gui class m

--- src/com/unipi/mainpackage/Admin.java
+++ src/com/unipi/mainpackage/Admin.java
@@ -7,5 +7,17 @@ public final class Admin extends User{
 		// TODO Auto-generated constructor stub
 	}
 
-	
+	public void createUser(String typeOfuser) {    //auto einai edw gt mono o admin kanei create user 
+		//from textboxes
+		String name = "alexhs v";
+		String username = "SavageAlexhs";
+		String password = "mlkas123";
+		if (typeOfuser.equals("Customer")){
+			String dateOfBirth = "24/09/2000";
+			Customer c = new Customer(name, username, password, dateOfBirth);
+		}
+		else if (typeOfuser.equals("ContentAdmin")) {
+			ContentAdmin ca = new ContentAdmin(name, username, password);
+		}
+	}
 }
